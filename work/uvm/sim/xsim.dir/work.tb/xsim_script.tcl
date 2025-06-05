35a3b1ce3a1430ec7593f9898221d9a3e68602e4
Nami-L <namigtle066@gmail.com>
2025-06-05 14:04:00 -0600

    FEAT:limpieza

--- work/uvm/sim/xsim.dir/work.tb/xsim_script.tcl
+++
@@ -1,4 +0,0 @@
-set_param project.enableReportConfiguration 0
-load_feature core
-current_fileset
-xsim {work.tb} -testplusarg UVM_TESTNAME=top_test -testplusarg UVM_VERBOSITY=UVM_DEBUG -wdb {/home/nami/time_alignment_2stage/work/uvm/wdb/tb.wdb} -autoloadwcfg -tclbatch {/home/nami/time_alignment_2stage/verification/uvm/scripts/vivado/run.tcl} -onerror stop -sv_seed 1
